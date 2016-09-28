1b1acb5f25fd25275a8477abe50249a13a5a1dae
Юрий Атрощенко <atr.yury@gmail.com>
2016-09-29 00:37:01 +0300

    Task 5 done.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # 5_lang_frequency
+The script is designed to find 10 most frequent words in the text.
+
+The script can work with texts written in any language.
+
+To run the script in console you should write:
+```bash
+python3 lang_frequency.py --path "./filename.txt"
+```
+or
+```bash
+python3 lang_frequency.py --path "/home/user/Documents/filename.txt"
+```
+Where **--path** the argument which assumes the script is a relative or full path to file with text.
+
+For this text and will be counted the frequency of words.
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,58 @@
-
+import argparse
 
 def load_data(filepath):
-    pass
+    try:
+        with open(filepath, 'r', encoding='utf-8') as f:
+            return f.read()
+    except Exception as error:
+        return error
 
+def getord(start, end):
+    return [x for x in range(start, end + 1)]
 
-def get_most_frequent_words(text):
-    pass
+EXCLUDE_CHARACTERS_ORD = [9, 10, 13] \
+    + getord(32, 47) + getord(58, 64) \
+    + getord(91, 96) + getord(123, 126) \
+    + getord(161, 191) + [8211]
 
+def mystrip(word=''):
+    if not word:
+        return None
+    if ord(word[:1]) in EXCLUDE_CHARACTERS_ORD:
+        word = word.strip(word[:1])
+        return mystrip(word)
+    if ord(word[len(word)-1:]) in EXCLUDE_CHARACTERS_ORD:
+        word = word.strip(word[len(word)-1:])
+        return mystrip(word)
+    return word
+
+def get_most_frequent_words(text, word_length=10):
+    words = {}
+    word = ''
+    for symbol in text:
+        symbol_ord = ord(symbol.lower())
+        if symbol_ord in [10, 13, 32] and word != '':
+            word = mystrip(word)
+            if word is not None:
+                words[word] = words.get(word, 0) + 1
+            word = ''
+        else:
+            if symbol_ord not in [9, 10, 13, 32]:
+                word += symbol.lower()
+    return list(sorted(words.items(), key=lambda x: x[1], reverse=True))[:word_length]
+
+def get_arguments():
+    args = argparse.ArgumentParser()
+    args.add_argument('--path', action='store', dest='filepath',
+                        help='Path to text file you want to calculate the frequency of words.')
+    return args.parse_args()
 
 if __name__ == '__main__':
-    pass
+    arguments = get_arguments()
+    data = load_data(arguments.filepath)
+    if isinstance(data, str):
+        word_length = 10
+        most_frequent_words = get_most_frequent_words(data, word_length)
+        print(most_frequent_words)
+    else:
+        print(data)
